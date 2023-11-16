4f2216ec44a36897a68503c106c7ab4bb26a51d7
LEGEARD Anthony <anthony.legeard@free.fr>
2023-11-16 09:36:29 +0100

    Code rebond laser tentative 1

---
+++ Assets/Scripts/Bullet/BulletRebond.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletRebond : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Prime"))
+        {
+
+        }
+    }
+}
