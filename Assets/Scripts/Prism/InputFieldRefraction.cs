b297368293c03185a810b1f50c88f80295567f1b
LEGEARD Anthony <anthony.legeard@free.fr>
2023-12-22 15:48:05 +0100

    Clamp input field indice refraction

--- Assets/Scripts/Prism/InputFieldRefraction.cs
+++ Assets/Scripts/Prism/InputFieldRefraction.cs
@@ -10,12 +10,11 @@ public class InputFieldRefraction : MonoBehaviour
     [SerializeField] private PrismTouchHandler touchHandler;
     private float actualInput;
 
-    private IndiceDeRefraction idrPrism;
-
     public void Start()
     {
         //Adds a listener to the main input field and invokes a method when the value changes.
         mainInputField.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+        mainInputField.text = touchHandler.selectedPrime.GetComponent<IndiceDeRefraction>().iDR.ToString();
     }
 
     // Invoked when the value of the text field changes.
@@ -23,6 +22,18 @@ public class InputFieldRefraction : MonoBehaviour
     {
         Debug.Log("Value Changed");
         float.TryParse(mainInputField.text,out actualInput);
+
+        if(actualInput < 1)
+        {
+            actualInput = 1;
+        }
+        if(actualInput > 3)
+        {
+            actualInput = 3;
+        }
+
+        mainInputField.text = actualInput.ToString();
+
         touchHandler.selectedPrime.GetComponent<IndiceDeRefraction>().iDR = actualInput;
     }
 }
