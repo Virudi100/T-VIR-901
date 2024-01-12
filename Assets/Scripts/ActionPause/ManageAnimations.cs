b61bd31c8105873435e004f1b32df5f99f306a73
ANAIS MEIRONE <84349590+anna8321@users.noreply.github.com>
2024-01-12 17:23:26 +0100

    button color

--- Assets/Scripts/ActionPause/ManageAnimations.cs
+++ Assets/Scripts/ActionPause/ManageAnimations.cs
@@ -60,30 +60,5 @@ public class ManageAnimations : MonoBehaviour
             Time.timeScale = 0f;
         }
     }
-    //private bool isPaused = false;
-
-    /*void Update()
-    {
-        // Ajoutez du code supplémentaire ici si nécessaire
-
-        // Gestion de la pause lorsque le bouton "Pause" est pressé
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            TogglePause();
-        }
-    }
-
-    public void TogglePause()
-    {
-        isPaused = !isPaused;
-
-        if (isPaused)
-        {
-            Time.timeScale = 0f; // Met en pause le temps
-        }
-        else
-        {
-            Time.timeScale = 1f; // Réactive le temps
-        }
-    }*/
+ 
 }
