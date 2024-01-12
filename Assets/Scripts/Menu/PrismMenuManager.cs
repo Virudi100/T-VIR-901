4d9bcfc92e7ce3d88298d2fa096bcb8a430461af
LEGEARD Anthony <anthony.legeard@free.fr>
2024-01-12 11:44:05 +0100

    Add shape & mat menu for prism

---
+++ Assets/Scripts/Menu/PrismMenuManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrismMenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject selectPrismButton;
+    [SerializeField] private GameObject selectPrismShape;
+    [SerializeField] private GameObject prismMatEmpty;
+    [SerializeField] private GameObject prismShapeEmpty;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectPrismButton.SetActive(true);
+        selectPrismShape.SetActive(true);
+        prismMatEmpty.SetActive(false);
+        prismShapeEmpty.SetActive(false);
+    }
+
+    public void ActivateShape()
+    {
+        prismShapeEmpty.SetActive(true);
+        prismMatEmpty.SetActive(false);
+
+        selectPrismButton.SetActive(false);
+        selectPrismShape.SetActive(false);
+    }
+
+    public void ActivateMat()
+    {
+        prismShapeEmpty.SetActive(false);
+        prismMatEmpty.SetActive(true);
+
+        selectPrismButton.SetActive(false);
+        selectPrismShape.SetActive(false);
+    }
+
+    public void DeactivateShape()
+    {
+        prismShapeEmpty.SetActive(false);
+
+        selectPrismButton.SetActive(true);
+        selectPrismShape.SetActive(true);
+    }
+
+    public void DeactivateMat()
+    {
+        prismMatEmpty.SetActive(false);
+
+        selectPrismButton.SetActive(true);
+        selectPrismShape.SetActive(true);
+    }
+}
